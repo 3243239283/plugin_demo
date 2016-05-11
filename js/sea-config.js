2f364c195b696c15c655288cafb03b2ec795bd06
3243239283 <treeclass@qq.com>
2016-05-11 19:38:55 +0800

    效果完善50%  16.5.11  19:38

--- js/sea-config.js
+++ js/sea-config.js
@@ -2,15 +2,19 @@
  * Created by Administrator on 2016/5/10.
  */
 seajs.config({
-    base:'../',
+	base:'../',
     alias: {
         'jquery': 'plugin_demo/plugin/jquery/jquery',
-        'seacssjs':'plugin_demo/plugin/seajs-3.0.0/seajs-css',
         'upload':'plugin_demo/js/upload',
-        //插件
+        //文件上传插件
         'uploadify':'plugin_demo/plugin/uploadify/jquery.uploadify',
         'uploadifycss':'plugin_demo/plugin/uploadify/uploadify.css'
 
-
     }
 });
+
+
+
+
+
+
--- js/upload.js
+++ js/upload.js
@@ -3,29 +3,15 @@
  */
 define(function(require,exprots,moudle){
     var $=require('jquery');
-    var uploadify=require('uploadify');
     var uploadifycss=require('uploadifycss');
+    var uploadify=require('uploadify');
+    
 
-
-    $("#uploadify").uploadify({
-        'uploader': 'plugin/plugin/uploadify/uploadify.swf',
-        'cancelImg': 'plugin/plugin/uploadify/cancel.png',
-        'queueID': 'fileQueue',
-        'auto': true,
-        'buttonText': 'select',
-        'simUploadLimit' : 1,
-        'queueSizeLimit' : 1,
-        'fileExt': '*.jpg;*.gif;*.jpeg;*.png;*.bmp;*.zip;*.rar;*.7z',
-        onComplete: function(event, queueID, fileObj, response, data) {
-            //转换为json对象
-            var dataObj = eval("("+response+")");
-            saveFile(dataObj);
-        },
-        onSelect:function(){
-        },
-        onError: function(event, queueID, fileObj) {
-            alert("文件:" + fileObj.name + "上传失败");
-        }
+    $("#file").uploadify({  
+    	'swf': 'plugin/uploadify/uploadify.swf',
+        'cancelImage': 'plugin_demo/plugin/uploadify/uploadify-cancel.png',
+        'multi': true,
+        'buttonText': '文件上传'
     });
 
 });
